bbf9be380bb3a9c2efc0b55f87350c6cdf01fefe
Alex <mail2zin@gmail.com>
2016-03-12 12:05:49 +0100

    bootstrap

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     console.log('header: dom ready');
 
+    $('#content').css('display', 'none');
+    $('#content').fadeIn(900);
+
     $('#header').load("header.html", function() {
         // list of all pages that used in navbar
         var pages = ['home', 'contact', 'sprites'];
@@ -15,6 +18,8 @@ $(document).ready(function() {
         }
     });
     $('#footer').load("footer.html");
+
+    //$('body').fadeOut(2000);
 
 
 
