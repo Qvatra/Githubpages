2deb893d0ec6818efaabdfe4bb331f232ab5b637
Alex <mail2zin@gmail.com>
2016-04-11 00:52:23 +0200

    php2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <noscript>
+  <meta http-equiv="refresh" content="0;URL='http://spritetools.com/'">
+</noscript>
+</head>
+
+<body>
+
+
+<script>
+    
+    console.log('ddddddd');
+    debugger;
+</script>
+
+</body>
+</html>
