403924fe6158265e106260d4576d19e79322c2a8
Alex <mail2zin@gmail.com>
2016-04-11 01:01:32 +0200

    2

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,4 +1,5 @@
 // is used for fast redirection if users use links to old joomla website
+console.log('welcome');
 if (location.pathname.indexOf('index.php') > -1) {
     location.href = 'index.html';
 } 
