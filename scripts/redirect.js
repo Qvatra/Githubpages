6e86d4e0c1c24f3447a54d9a3c4ebadead97fa90
Alex <mail2zin@gmail.com>
2016-04-15 23:21:35 +0200

    revert last

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,4 +1,4 @@
 // is used for fast redirection if users use links to old joomla website
 if (location.pathname.indexOf('index.php') > -1) {
-    location.href = 'https://spritetools.com/index.html';
+    location.href = 'https://qvatra.github.io/Githubpages/index.html';
 }
